1841c751088c57e32ea398155d8a477482cdefbd
Zack Leach <zack.a.leach@gmail.com>
2022-02-14 22:41:17 -0700

    Update sketch.js

--- Scripts/004/sketch.js
+++ Scripts/004/sketch.js
@@ -58,7 +58,7 @@ function playerChar(){
   let d = 68;
   let Lmouse = 1;
 
-  let ship = player(player.x,player.y);
+  let ship = player;
   image (shipImg,player.x,player.y);
   //rotate to follow mouse
   translate(player.x, player.y);
